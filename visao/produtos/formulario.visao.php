834ac8c569a4024309fcf6d6b12181bcdae4fa85
thaliasilv <47746678+thaliasilv@users.noreply.github.com>
2019-06-03 09:21:36 -0300

    Add files via upload

--- visao/produtos/formulario.visao.php
+++ visao/produtos/formulario.visao.php
@@ -1,8 +1,10 @@
 <form action="" method="POST">
-    Nome do Produto: <input type="text" name=" NomeProduto">
-    Codigo do Produto:<input type="text" name="CodigoProduto">
-    Descriçao do Produto:<input type="text" name="DescriProduto">
-    Quantidade do Produto: <input type="text" name="QuantidadeProduto">
-    Preço do produto: <input type="text" name="PreProduto">
+    Nome do Produto: <input type="text" name=" NomeProduto"><br><br>
+    Codigo do Produto:<input type="text" name="CodigoProduto"><br><br>
+    Categoria do Produto:<input type="text" name="CategoriaProduto"><br><br>
+    Codigo do fabricante:<input type="text" name="CodFabricante"><br><br>
+    Descriçao do Produto:<input type="text" name="DescriProduto"><br><br>
+    Quantidade do Produto: <input type="text" name="QuantidadeProduto"><br><br>
+    Preço do produto: <input type="text" name="PreProduto"><br><br><br><br>
     <button type="submit">Enviar</button>
 </form>
