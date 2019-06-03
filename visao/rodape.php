fe2daefbd55072827e343f9dbc2af573e0156d7a
thaliasilv <47746678+thaliasilv@users.noreply.github.com>
2019-06-03 10:52:21 -0300

    Add files via upload

---
+++ visao/rodape.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head><title>RODAPÉ</title>
+  
+<style>
+.mySlides {display:none;}
+</style>
+</head>
+<body>
+<div style="margin-top: 15%; background-color: #DBA901" id="rodape">
+    <div id="rc">
+      <div class="tr">
+        
+        <p class="dp">Instituto Federal de Educação, Ciência e Tecnologia de São Paulo, Campus Itapetininga</p>
+      </div>
+      <div class="tr">
+        <p class="dp">Endereço: Av. João Olímpio de Oliveira, 1561 - Vila Asem, Itapetininga - SP, 18202-000</p>
+      </div>
+
+
+
+      <div class="tr">
+      	<a  style="padding: 1%; margin-right: 4%; margin-bottom: 4%" href="administrador.html"><i class="user outline icon"></i> Administrador</a>
+       
+        <p class="dp">Telefone para contato: (15) 3376-9930</p>
+      </div>
+    </div>  
+      <div class="linro"></div>
+    <div id="rb">
+      <p id="da">©Untitled.   All rights reserved.   Designers: Adrielly e Thalia.    Ifsp Câmpus Itapetininga 2018.</p>
+    
+    </div>
+  </div>
+  
+<link rel="stylesheet" type="text/css" href="semantic.min.jv">
+</body>
+</html>
